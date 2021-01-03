4d436fe0821383e925a514f0b22ba76683427052
KristianP26 <partl.kristian@gmail.com>
2021-01-03 03:35:05 +0100

    final

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta name="description" content="SurfCommunity">
         <meta name="author" content="Kristián Partl">
         
-        <title>Surf Community</title>
+        <title>surfcommunity.eu</title>
 
         <link rel="icon" href="./images/favicon.svg" type="image">
 
@@ -30,34 +30,38 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <div class="text-center text-white my-5">
-                            <img src="images/logo.svg" height="150" width="100%" class=my-3>
+                        <div class="text-center text-white mt-md-5 mt-0">
+                            <img src="images/logo.svg" height="auto" width="100%" class="my-md-4 my-3 mt-md-5 mt-0">
                             <h4 class="">
-                                Surf Community is a CS:GO community server group for 102.4 tick Surf mode.
+                                    Surf Community is a CS:GO community server group for 102.4 tick Surf mode.
                                 <br>
-                                We were established in the first quarter of 2020.
+                                    We were established in the first quarter of 2020.
                                 <br>
                                 <small>
-                                We are currently trying to improve our website and increase number of maps on our servers.
+                                    We are currently trying to improve our website and increase number of maps on our servers.
+                                    <br>
+                                    <span class="text-success">New Website Comming soon.</span>
                                 </small>
                             </h4>
                             <ul class="nav justify-content-center my-5">
-                                <li class="nav-item mx-2">
+                                <li class="nav-item col-md col-12 my-md-0 my-1">
                                     <a class="nav-link btn btn-outline-light" href="https://surfcommunity.eu/websites/old/servers.php?sid=1"><i class="fas fa-server"></i> Server List</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item col-md col-12 my-md-0 my-1">
                                     <a class="nav-link btn btn-outline-light" role="button" href="https://surfcommunity.eu/websites/Surftimer-Web-Stats/"><i class="far fa-chart-bar"></i> Surf Stats</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item col-md col-12 my-md-0 my-1">
                                     <a class="nav-link btn btn-outline-light disabled" href="#"><i class="fas fa-globe-europe"></i> New Website</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item col-md col-12 my-md-0 my-1">
                                     <a class="nav-link btn btn-outline-light" href="https://surfcommunity.eu/websites/old/"><i class="fas fa-globe-europe"></i> Old Website</a>
                                 </li>
-                                <li class="nav-item mx-2">
+                                <li class="nav-item col-md col-12 my-md-0 my-1">
                                     <a class="nav-link btn btn-outline-light" href="https://discord.gg/cCrz56qJWF" target="_blank"><i class="fab fa-discord"></i> Discord</a>
                                 </li>
                             </ul>
+                            <hr>
+                            <p class="text-muted"><small>Copyright © 2021 - surfcommunity.eu<br>Coded & Designed with ❤ by Kristián Partl.</small></p>
                         </div>
                     </div>
                 </div>
