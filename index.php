72a62e2a5eb2a5f64086d719e80019893c044dde
KristianP26 <partl.kristian@gmail.com>
2021-01-03 03:04:02 +0100

    Creating Website

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="SurfCommunity">
+        <meta name="author" content="Kristián Partl">
+        
+        <title>Surf Community</title>
+
+        <link rel="icon" href="./images/favicon.svg" type="image">
+
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom core CSS -->
+        <link href="vendor/fontawesome/css/all.min.css" rel="stylesheet"> 
+        <link href="vendor/css/custom.css" rel="stylesheet">
+
+        <script src="vendor/js/jquery-3.5.1.slim.min.js"></script>
+        <script src="vendor/js/popper.min.js"></script>
+        <script src="vendor/js/bootstrap.min.js"></script>
+
+    </head>
+
+    <body class="d-flex flex-column">
+        <div id="page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center text-white my-5">
+                            <img src="images/logo.svg" height="150" width="100%" class=my-3>
+                            <h4 class="">
+                                Surf Community is a CS:GO community server group for 102.4 tick Surf mode.
+                                <br>
+                                We were established in the first quarter of 2020.
+                                <br>
+                                <small>
+                                We are currently trying to improve our website and increase number of maps on our servers.
+                                </small>
+                            </h4>
+                            <ul class="nav justify-content-center my-5">
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link btn btn-outline-light" href="https://surfcommunity.eu/websites/old/servers.php?sid=1"><i class="fas fa-server"></i> Server List</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link btn btn-outline-light" role="button" href="https://surfcommunity.eu/websites/Surftimer-Web-Stats/"><i class="far fa-chart-bar"></i> Surf Stats</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link btn btn-outline-light disabled" href="#"><i class="fas fa-globe-europe"></i> New Website</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link btn btn-outline-light" href="https://surfcommunity.eu/websites/old/"><i class="fas fa-globe-europe"></i> Old Website</a>
+                                </li>
+                                <li class="nav-item mx-2">
+                                    <a class="nav-link btn btn-outline-light" href="https://discord.gg/cCrz56qJWF" target="_blank"><i class="fab fa-discord"></i> Discord</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
